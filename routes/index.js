67a91f7ed790e719681f585ba395ba282563040f
Aaryan <aaryanofficial64@gmail.com>
2026-02-19 18:11:19 +0530

    restructure and management

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
-// Mount other route files
+// Mount tournament routes
 const tournamentRoutes = require("./tournaments.js");
-const contactRoutes = require("./contact.js");
 
-router.get("/" , (req,res) => {
-    res.render('home.ejs') ;
-}) ;
-router.get("/login" , (req,res) => {
-    res.render('login.ejs') ;
-}) ;
-router.get("/register" , (req,res) => {
-    res.render('register.ejs') ;
-}) ;
+// Home Page
+router.get("/", (req, res) => {
+    res.render('home.ejs');
+});
 
+// Auth Routes (Your teammate will handle)
+router.get("/login", (req, res) => res.render('login.ejs'));
+router.get("/register", (req, res) => res.render('register.ejs'));
 
-// Mount routes with prefixes
+// Static Pages - FIXED PATHS
+router.get("/about", (req, res) => {
+    res.render("about.ejs");
+});
+
+router.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+});
+
+// Tournament Routes
 router.use("/tournaments", tournamentRoutes);
-router.use("/contact", contactRoutes);
-router.get("/about" , (req,res) => {
-    res.render("tournaments/about.ejs") ;
-}) ;
 
 module.exports = router;
